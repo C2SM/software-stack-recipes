7358df248c5329a19377f876bc6aa63898757f51
Carlos Osuna <carlos.osuna@meteoswiss.ch>
2025-07-13 00:52:01 +0200

    expand params

---
+++ recipes/fdb/5.16/meta/private/expand_params.py
@@ -0,0 +1,44 @@
+import argparse
+import re 
+import yaml 
+
+def parse_file(filepath):
+    entries = {}
+
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+
+    for i in range(len(lines)):
+        # Look for lines with paramId
+        param_match = re.match(r"#paramId:\s*(\d+)", lines[i])
+        if param_match:
+            param_id = param_match.group(1)
+            description = lines[i + 1].strip().lstrip("#").strip()
+            key_match = re.match(r"'([^']+)'", lines[i + 2])
+            key = key_match.group(1) if key_match else ""
+            entries[param_id] = ((key, description))
+
+    return entries
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Process two filenames.")
+    parser.add_argument("paramids", type=str, help="Path to the first file")
+    parser.add_argument("shortnamedef", type=str, help="Path to the second file")
+    
+    args = parser.parse_args()
+    
+    icon_params = parse_file(args.shortnamedef)
+
+    with open(args.paramids, 'r') as file:
+        metkit_params = yaml.safe_load(file)
+
+    for key in icon_params:
+        if int(key) not in metkit_params:
+            metkit_params[int(key)] = tuple(s.lower() for s in icon_params[key]) 
+
+    with open(args.paramids, 'w') as file:
+        yaml.dump(metkit_params, file, sort_keys=True)
+
+if __name__ == "__main__":
+    main()
