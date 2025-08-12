5ed9cd8bc265c2b08c29c1b31a5ac3791cfbeaa2
cosunae <carlos.osuna@meteoswiss.ch>
2025-08-12 17:06:57 +0200

    expand the language.yaml to add the meteoswiss models (#15)

---
+++ recipes/fdb/5.17/meta/private/expand_language.py
@@ -0,0 +1,64 @@
+import argparse
+import yaml
+
+
+def convert_nested_lists(obj):
+    if isinstance(obj, dict):
+        return {k: convert_nested_lists(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        # If it's a list of lists, convert inner lists to tuples
+        return [
+            (
+                tuple(convert_nested_lists(item))
+                if isinstance(item, list)
+                else convert_nested_lists(item)
+            )
+            for item in obj
+        ]
+    else:
+        return obj
+
+
+class NoAliasDumper(yaml.SafeDumper):
+    def ignore_aliases(self, data):
+        return True
+
+
+# Force tuples to be dumped in flow style
+def tuple_representer(dumper, data):
+    return dumper.represent_sequence("tag:yaml.org,2002:seq", data, flow_style=True)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="expand the metkit language.yaml for the meteoswiss mars model"
+    )
+    parser.add_argument("langyaml", type=str, help="Path to the first file")
+
+    args = parser.parse_args()
+
+    with open(args.langyaml, "r") as file:
+        metkit_lang = yaml.safe_load(file)
+
+    metkit_lang["_field"]["model"]["type"].append(
+        {"type": "enum", "values": [["icon-ch1-eps"], ["icon-ch2-eps"]]}
+    )
+
+    yaml.add_representer(tuple, tuple_representer, Dumper=NoAliasDumper)
+    # convert list to tuples so that the yaml dump generates [] (original formatting of the yaml) instead of rows of "-"
+    metkit_lang = convert_nested_lists(metkit_lang)
+
+    with open(args.langyaml, "w") as file:
+        yaml.dump(
+            metkit_lang,
+            file,
+            sort_keys=False,
+            default_flow_style=False,
+            allow_unicode=True,
+            Dumper=NoAliasDumper,
+            width=float("inf"),
+        )
+
+
+if __name__ == "__main__":
+    main()
